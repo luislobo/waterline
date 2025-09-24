babbac509e7f3452d517fb6faaa0f8648678388e
Luis Lobo Borobia <luis.lobo@epicio.com>
2025-09-24 13:11:44 -0500

    feat(populate): support select/omit projections
    ## Summary
    
    - capture original populate subcriteria in help-find and apply select/omit filters after unserializing populated records so adapters that ignore projections still return trimmed data
    
    - allow singular populate subcriteria to specify select/omit in forge-stage-two-query with strong validation (arrays only, known/non-collection attributes, preserved child PK, schema:false restrictions)
    
    - add comprehensive regression tests for valid projections and every E_INVALID_POPULATES branch, adapted from sailscastshq/waterline@414c9275
    
    ## Testing
    
    - npm test

--- test/unit/query/associations/populateArray.js
+++ test/unit/query/associations/populateArray.js
@@ -3,10 +3,11 @@ var Waterline = require('../../../../lib/waterline');
 
 describe('Collection Query ::', function() {
   describe('specific populated associations ::', function() {
+    var waterline;
     var Car;
 
     before(function(done) {
-      var waterline = new Waterline();
+      waterline = new Waterline();
       var collections = {};
 
       collections.user = Waterline.Model.extend({
@@ -68,20 +69,22 @@ describe('Collection Query ::', function() {
       waterline.registerModel(collections.car);
       waterline.registerModel(collections.ticket);
 
-      // Fixture Adapter Def
       var adapterDef = {
         identity: 'foo',
         find: function(con, query, cb) {
-          if(query.using === 'user') {
+          if (query.using === 'user') {
             return cb(null, [{ id: 1, car: 1, name: 'John Doe' }]);
           }
 
-          if(query.using === 'car') {
+          if (query.using === 'car') {
             return cb(null, [{ id: 1, foobar: 1, tickets: [1, 2]}]);
           }
 
-          if(query.using === 'ticket') {
-            return cb(null, [{ id: 1, reason: 'red light', car:1}, { id: 2, reason: 'Parking in a disabled space', car: 1 }]);
+          if (query.using === 'ticket') {
+            return cb(null, [
+              { id: 1, reason: 'red light', car: 1 },
+              { id: 2, reason: 'Parking in a disabled space', car: 1 }
+            ]);
           }
 
           return cb();
@@ -89,7 +92,7 @@ describe('Collection Query ::', function() {
       };
 
       var connections = {
-        'foo': {
+        foo: {
           adapter: 'foobar'
         }
       };
@@ -100,11 +103,20 @@ describe('Collection Query ::', function() {
         }
 
         Car = orm.collections.car;
-
         return done();
       });
     });
 
+    after(function(done) {
+      if (!waterline) {
+        return done();
+      }
+
+      waterline.teardown(function() {
+        waterline = null;
+        return done();
+      });
+    });
 
     it('should populate all related collections', function(done) {
       Car.find()
@@ -120,6 +132,407 @@ describe('Collection Query ::', function() {
         assert(car[0].tickets);
         assert(car[0].tickets[0].car);
         assert(car[0].tickets[1].car);
+        return done();
+      });
+    });
+
+    it('should allow populate with select subcriteria for singular associations', function(done) {
+      Car.find()
+      .populate('driver', {
+        select: ['name']
+      })
+      .exec(function(err, car) {
+        if (err) {
+          return done(err);
+        }
+
+        assert(car[0].driver);
+        assert.strictEqual(typeof car[0].driver.name, 'string');
+        assert.strictEqual(typeof car[0].driver.id, 'number');
+        assert.strictEqual(Object.keys(car[0].driver).length, 2);
+        return done();
+      });
+    });
+
+    it('should allow populate with omit subcriteria for singular associations', function(done) {
+      Car.find()
+      .populate('driver', {
+        omit: ['car']
+      })
+      .exec(function(err, car) {
+        if (err) {
+          return done(err);
+        }
+
+        assert(car[0].driver);
+        assert.strictEqual(typeof car[0].driver.name, 'string');
+        assert.strictEqual(typeof car[0].driver.id, 'number');
+        assert.strictEqual(car[0].driver.car, undefined);
+        return done();
+      });
+    });
+
+    it('should allow populate with empty object (converts to true)', function(done) {
+      Car.find()
+      .populate('driver', {})
+      .exec(function(err, car) {
+        if (err) {
+          return done(err);
+        }
+
+        assert(car[0].driver);
+        assert.strictEqual(typeof car[0].driver.name, 'string');
+        return done();
+      });
+    });
+
+    it('should throw error when both select and omit are provided together', function(done) {
+      Car.find()
+      .populate('driver', {
+        select: ['name'],
+        omit: ['id']
+      })
+      .exec(function(err) {
+        assert(err);
+        assert.equal(err.code, 'E_INVALID_POPULATES');
+        assert(err.message.indexOf('providing both `select` AND `omit`') > -1);
+        return done();
+      });
+    });
+  });
+
+  describe('populate subcriteria validation errors ::', function() {
+    var waterline;
+    var Holder;
+
+    before(function(done) {
+      waterline = new Waterline();
+      var collections = {};
+
+      collections.pet = Waterline.Model.extend({
+        identity: 'pet',
+        datastore: 'foo',
+        primaryKey: 'id',
+        attributes: {
+          id: {
+            type: 'number'
+          },
+          name: {
+            type: 'string'
+          },
+          owner: {
+            model: 'buddy'
+          }
+        }
+      });
+
+      collections.buddy = Waterline.Model.extend({
+        identity: 'buddy',
+        datastore: 'foo',
+        primaryKey: 'id',
+        attributes: {
+          id: {
+            type: 'number'
+          },
+          name: {
+            type: 'string'
+          },
+          nickname: {
+            type: 'string'
+          },
+          pets: {
+            collection: 'pet',
+            via: 'owner'
+          }
+        }
+      });
+
+      collections.holder = Waterline.Model.extend({
+        identity: 'holder',
+        datastore: 'foo',
+        primaryKey: 'id',
+        attributes: {
+          id: {
+            type: 'number'
+          },
+          buddy: {
+            model: 'buddy'
+          }
+        }
+      });
+
+      waterline.registerModel(collections.holder);
+      waterline.registerModel(collections.buddy);
+      waterline.registerModel(collections.pet);
+
+      var adapterDef = {
+        identity: 'foo',
+        find: function(con, query, cb) {
+          if (query.using === 'holder') {
+            return cb(null, [{ id: 1, buddy: 1 }]);
+          }
+
+          if (query.using === 'buddy') {
+            return cb(null, [{ id: 1, name: 'Buddy One', nickname: 'B', pets: [] }]);
+          }
+
+          if (query.using === 'pet') {
+            return cb(null, []);
+          }
+
+          return cb();
+        }
+      };
+
+      var connections = {
+        foo: {
+          adapter: 'foobar'
+        }
+      };
+
+      waterline.initialize({ adapters: { foobar: adapterDef }, datastores: connections }, function(err, orm) {
+        if (err) {
+          return done(err);
+        }
+
+        Holder = orm.collections.holder;
+        return done();
+      });
+    });
+
+    after(function(done) {
+      if (!waterline) {
+        return done();
+      }
+
+      waterline.teardown(function() {
+        waterline = null;
+        return done();
+      });
+    });
+
+    it('should error when populate select clause is not an array', function(done) {
+      Holder.find()
+      .populate('buddy', {
+        select: 'name'
+      })
+      .exec(function(err) {
+        assert(err);
+        assert.equal(err.code, 'E_INVALID_POPULATES');
+        assert(err.message.indexOf('invalid `select` clause') > -1);
+        return done();
+      });
+    });
+
+    it('should error when populate select clause contains a non-string', function(done) {
+      Holder.find()
+      .populate('buddy', {
+        select: ['name', 5]
+      })
+      .exec(function(err) {
+        assert(err);
+        assert.equal(err.code, 'E_INVALID_POPULATES');
+        assert(err.message.indexOf('not a string') > -1);
+        return done();
+      });
+    });
+
+    it('should error when populate select clause references an unknown attribute', function(done) {
+      Holder.find()
+      .populate('buddy', {
+        select: ['name', 'age']
+      })
+      .exec(function(err) {
+        assert(err);
+        assert.equal(err.code, 'E_INVALID_POPULATES');
+        assert(err.message.indexOf('not a recognized attribute') > -1);
+        return done();
+      });
+    });
+
+    it('should error when populate select clause references a collection attribute', function(done) {
+      Holder.find()
+      .populate('buddy', {
+        select: ['name', 'pets']
+      })
+      .exec(function(err) {
+        assert(err);
+        assert.equal(err.code, 'E_INVALID_POPULATES');
+        assert(err.message.indexOf('plural ("collection") association') > -1);
+        return done();
+      });
+    });
+
+    it('should error when populate omit clause is not an array', function(done) {
+      Holder.find()
+      .populate('buddy', {
+        omit: 'nickname'
+      })
+      .exec(function(err) {
+        assert(err);
+        assert.equal(err.code, 'E_INVALID_POPULATES');
+        assert(err.message.indexOf('invalid `omit` clause') > -1);
+        return done();
+      });
+    });
+
+    it('should error when populate omit clause contains a non-string', function(done) {
+      Holder.find()
+      .populate('buddy', {
+        omit: [3]
+      })
+      .exec(function(err) {
+        assert(err);
+        assert.equal(err.code, 'E_INVALID_POPULATES');
+        assert(err.message.indexOf('not a string') > -1);
+        return done();
+      });
+    });
+
+    it('should error when populate omit clause references an unknown attribute', function(done) {
+      Holder.find()
+      .populate('buddy', {
+        omit: ['age']
+      })
+      .exec(function(err) {
+        assert(err);
+        assert.equal(err.code, 'E_INVALID_POPULATES');
+        assert(err.message.indexOf('not a recognized attribute') > -1);
+        return done();
+      });
+    });
+
+    it('should error when populate omit clause references the associated model primary key', function(done) {
+      Holder.find()
+      .populate('buddy', {
+        omit: ['id']
+      })
+      .exec(function(err) {
+        assert(err);
+        assert.equal(err.code, 'E_INVALID_POPULATES');
+        assert(err.message.indexOf('primary key') > -1);
+        return done();
+      });
+    });
+
+    it('should error when populate omit clause references a collection attribute', function(done) {
+      Holder.find()
+      .populate('buddy', {
+        omit: ['pets']
+      })
+      .exec(function(err) {
+        assert(err);
+        assert.equal(err.code, 'E_INVALID_POPULATES');
+        assert(err.message.indexOf('plural ("collection") association') > -1);
+        return done();
+      });
+    });
+  });
+
+  describe('populate subcriteria validation with schema false associated model ::', function() {
+    var waterline;
+    var Haunter;
+
+    before(function(done) {
+      waterline = new Waterline();
+      var collections = {};
+
+      collections.ghost = Waterline.Model.extend({
+        identity: 'ghost',
+        datastore: 'foo',
+        primaryKey: 'id',
+        schema: false,
+        attributes: {
+          id: {
+            type: 'number'
+          },
+          name: {
+            type: 'string'
+          }
+        }
+      });
+
+      collections.haunter = Waterline.Model.extend({
+        identity: 'haunter',
+        datastore: 'foo',
+        primaryKey: 'id',
+        attributes: {
+          id: {
+            type: 'number'
+          },
+          friend: {
+            model: 'ghost'
+          }
+        }
+      });
+
+      waterline.registerModel(collections.haunter);
+      waterline.registerModel(collections.ghost);
+
+      var adapterDef = {
+        identity: 'foo',
+        find: function(con, query, cb) {
+          if (query.using === 'haunter') {
+            return cb(null, [{ id: 1, friend: 1 }]);
+          }
+
+          if (query.using === 'ghost') {
+            return cb(null, [{ id: 1, name: 'Boo' }]);
+          }
+
+          return cb();
+        }
+      };
+
+      var connections = {
+        foo: {
+          adapter: 'foobar'
+        }
+      };
+
+      waterline.initialize({ adapters: { foobar: adapterDef }, datastores: connections }, function(err, orm) {
+        if (err) {
+          return done(err);
+        }
+
+        Haunter = orm.collections.haunter;
+        return done();
+      });
+    });
+
+    after(function(done) {
+      if (!waterline) {
+        return done();
+      }
+
+      waterline.teardown(function() {
+        waterline = null;
+        return done();
+      });
+    });
+
+    it('should error when populate select clause is used with schema false associated model', function(done) {
+      Haunter.find()
+      .populate('friend', {
+        select: ['name']
+      })
+      .exec(function(err) {
+        assert(err);
+        assert.equal(err.code, 'E_INVALID_POPULATES');
+        assert(err.message.indexOf('declared `schema: false`') > -1);
+        return done();
+      });
+    });
+
+    it('should error when populate omit clause is used with schema false associated model', function(done) {
+      Haunter.find()
+      .populate('friend', {
+        omit: ['name']
+      })
+      .exec(function(err) {
+        assert(err);
+        assert.equal(err.code, 'E_INVALID_POPULATES');
+        assert(err.message.indexOf('declared `schema: false`') > -1);
         return done();
       });
     });
